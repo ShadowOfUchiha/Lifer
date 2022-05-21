e57375d39b83216af0ecb0c7367cbf630c1c56b9
johnny <johnnymihai33@gmail.com>
2022-05-21 15:11:18 +0200

    added 14 more color themes

--- wp-content/themes/Lifer/assets/js/main.js
+++ wp-content/themes/Lifer/assets/js/main.js
@@ -3,14 +3,14 @@ function change_theme($theme) {
 
     switch ($theme) {
         case "Groen-Roze":
-            $bgColor = "#66ff00";
+            $bgColor = "#65ff00";
             $color = "#ff006f";
             document.getElementById("homeBtn").classList.add('btn-pink');
             break;
-        case "Paars-Geel":
+        case "Paars-Groen":
             $bgColor = "#9100ff";
-            $color = "#bfff00";
-            document.getElementById("homeBtn").classList.add('btn-yellow');
+            $color = "#a0d700";
+            document.getElementById("homeBtn").classList.add('btn-green');
             break;
         case "Oranje-Aqua":
             $bgColor = "#ffdb00";
@@ -18,23 +18,88 @@ function change_theme($theme) {
             document.getElementById("homeBtn").classList.add('btn-aqua');
             break;
         case "Roze-Blauw":
-            $bgColor = "#fb0484";
-            $color = "#1004c3";
+            $bgColor = "#ff0087";
+            $color = "#2601e7";
             document.getElementById("homeBtn").classList.add('btn-blue');
             break;
+        case "Oranje-Lichtpaars":
+            $bgColor = "#ff7101";
+            $color = "#5600d7";
+            document.getElementById("homeBtn").classList.add('btn-lightpurple');
+            break;
+        case "Geel-Paars":
+            $bgColor = "#fed000";
+            $color = "#3d0077";
+            document.getElementById("homeBtn").classList.add('btn-purple');
+            break;
+        case "Lichtroze-Lichtblauw":
+            $bgColor = "#ff00fc";
+            $color = "#0088fe";
+            document.getElementById("homeBtn").classList.add('btn-lightblue');
+            break;
+        case "Blauw-Oranje":
+            $bgColor = "#008ac3";
+            $color = "#ff723d";
+            document.getElementById("homeBtn").classList.add('btn-orange');
+            break;
+        case "Oceaanblauw-Lichtoranje":
+            $bgColor = "#1c00a0";
+            $color = "#ff9501";
+            document.getElementById("homeBtn").classList.add('btn-lightorange');
+            break;
+        case "Muntgroen-Donkerroze":
+            $bgColor = "#69ff94";
+            $color = "#760147";
+            document.getElementById("homeBtn").classList.add('btn-darkpink');
+            break;
+        case "Cyan-Lichtroze":
+            $bgColor = "#760147";
+            $color = "#d700d7";
+            document.getElementById("homeBtn").classList.add('btn-lightpink');
+            break;
+        case "Oranjerood-Muntgroen":
+            $bgColor = "#fe0000";
+            $color = "#00fa74";
+            document.getElementById("homeBtn").classList.add('btn-mintgreen');
+            break;
+        case "Olijfgroen-Donkergeel":
+            $bgColor = "#187a15";
+            $color = "#d6c109";
+            document.getElementById("homeBtn").classList.add('btn-darkyellow');
+            break;
+        case "Donkerroze-Kiwigroen":
+            $bgColor = "#8b005d";
+            $color = "#5acb0f";
+            document.getElementById("homeBtn").classList.add('btn-kiwi');
+            break;
+        case "Roze-Bladgroen":
+            $bgColor = "#fe678a";
+            $color = "#1ea020";
+            document.getElementById("homeBtn").classList.add('btn-leafgreen');
+            break;
+        case "Bruin-Luchtblauw":
+            $bgColor = "#7b0002";
+            $color = "#2dbaba";
+            document.getElementById("homeBtn").classList.add('btn-sky');
+            break;
         default:
-            $bgColor = "#fb0484";
-            $color = "#1004c3";
+            $bgColor = "#ff0087";
+            $color = "#2601e7";
             document.getElementById("homeBtn").classList.add('btn-blue');
             break;
     }
 
+    // body
     document.body.style.backgroundColor = $bgColor;
+    // title
     document.getElementById('home').style.color = $color;
+    // challenge #1
     document.getElementById('counter').style.color = $color;
 
-    // If you have just one class for the element
+    // all <p> elements color
     document.querySelectorAll('p').forEach(e => e.style.color = $color);
+    // hamburger menu bgcolor
+    document.querySelectorAll('.spinner').forEach(e => e.style.backgroundColor = $color);
 
 }
 
